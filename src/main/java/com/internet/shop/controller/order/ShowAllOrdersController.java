a086bbca8a6da5a1fc6bd6c893edde4658942bd3
SetrinaOlesia <setrina527@gmail.com>
2020-09-15 21:20:24 +0300

    added jsp page

---
+++ src/main/java/com/internet/shop/controller/order/ShowAllOrdersController.java
@@ -0,0 +1,24 @@
+package com.internet.shop.controller.order;
+
+import com.internet.shop.lib.Injector;
+import com.internet.shop.service.OrderService;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/admin/orders")
+public class ShowAllOrdersController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("com.internet.shop");
+    private final OrderService orderService =
+            (OrderService) injector.getInstance(OrderService.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.setAttribute("orders", orderService.getAll());
+        req.getRequestDispatcher("/WEB-INF/views/orders/allOrders.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>Home Page</title>
+</head>
+<body>
+<%@include file="header.jsp"%>
+<h1 style="font-family: 'Monotype Corsiva',fantasy">Hello World!</h1>
+</body>
+</html>
